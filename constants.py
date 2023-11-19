eb09d8c7f4cdf1cb970b28b70cf9ba1b8a95dae6
wermos <63574588+wermos@users.noreply.github.com>
2023-11-19 19:59:39 +0530

    Added a file full of constants that the other files use.

---
+++ constants.py
@@ -0,0 +1,28 @@
+from math import floor
+
+# The number of images we have of each object
+NUM_IMAGES = 72
+
+# Number of pixels in one image
+IMAGE_HEIGHT = 128
+IMAGE_WIDTH = 128
+IMAGE_SIZE = IMAGE_HEIGHT * IMAGE_WIDTH
+
+TRAINING_PERCENTAGE = 0.8
+
+# This value is set according to the value of `TRAINING_PERCENTAGE` in case it is needed.
+TESTING_PERCENTAGE = 1 - TRAINING_PERCENTAGE
+
+# We will designate approximately `TRAINING_PERCENTAGE` of the dataset to training and the
+# remaining `TESTING_PERCENTAGE` to testing.
+
+# Since there are `NUM_IMAGES` images of each object, and we use `floor(72 * TRAINING_PERCENTAGE)` images
+# for training, and the remaining images for testing.
+#
+# The training and testing images will be randomly chosen to provide more
+# variance in the angle and orientation of the testing images.
+
+NUM_TRAINING_IMAGES = floor(NUM_IMAGES * TRAINING_PERCENTAGE)
+
+# Once again, this value is set according to the value of `NUM_TRAINING_IMAGES` in case it is needed.
+NUM_TESTING_IMAGES = NUM_IMAGES - NUM_TRAINING_IMAGES
