5ec8f54d4155a1e442a66558c76ac4374f4e714f
paramrathour <rathourparam3435@gmail.com>
2023-11-20 12:56:00 +0530

    improvised image matrix generation code

--- algorithm.py
+++ algorithm.py
@@ -6,32 +6,22 @@ from data_loader import coil_20_data_loader
 if __name__ == "__main__":
     training, testing = coil_20_data_loader()
 
-    vectors_universal = [[] for i in range(NUM_OBJECTS*NUM_TRAINING_IMAGES)]
-    vectors_object = [[] for i in range(NUM_OBJECTS)]
-    object_angles = [[] for i in range(NUM_OBJECTS)]
-    for object_id, angle, image  in training:
-        vectors_object[object_id].append(image)
-        object_angles[object_id].append(angle)
+    vectors_universal = np.zeros((IMAGE_SIZE, NUM_OBJECTS*NUM_TRAINING_IMAGES))
+    vectors_object = np.zeros((NUM_OBJECTS, IMAGE_SIZE, NUM_TRAINING_IMAGES))
+    object_angles = np.zeros((NUM_OBJECTS, NUM_TRAINING_IMAGES))
+    for idx, (object_id, angle, image)  in enumerate(training):
+        image = image / np.linalg.norm(image)
+        image = np.squeeze(image)
+        vectors_universal[:, idx] = image
+        vectors_object[object_id, :, idx % NUM_TRAINING_IMAGES] = image
+        object_angles[object_id][idx % NUM_TRAINING_IMAGES] = angle
 
-    for i in range(NUM_OBJECTS):
-        vectors_universal[NUM_TRAINING_IMAGES*i:NUM_TRAINING_IMAGES*i+NUM_TRAINING_IMAGES] = vectors_object[i]
+    mean_universal = np.mean(vectors_universal, axis = 1)
+    X_universal = (vectors_universal.T - mean_universal).T
 
-    vectors_universal = np.squeeze(np.array(vectors_universal))
-    vectors_object = np.squeeze(np.array(vectors_object))
-    object_angles = np.squeeze(np.array(object_angles))
-
-    mean_universal = np.mean(vectors_universal, axis = 0)
-    mean_object = np.empty(NUM_OBJECTS, dtype = object)
-    for i, vectors in enumerate(vectors_object):
-        mean_object[i] = np.mean(vectors, axis = 0)
-
-    X_universal = vectors_universal - mean_universal
-    X_object = np.empty(NUM_OBJECTS, dtype = object)
-    for i in range(NUM_OBJECTS):
-        X_object[i] = vectors_object[i] - mean_object[i]
-
-    X_universal = np.matrix(X_universal)
-    for i in range(20):
-        X_object[i] = np.matrix(X_object[i])
+    mean_object = np.zeros((IMAGE_SIZE, NUM_OBJECTS))
+    for i, object_vectors in enumerate(vectors_object):
+        mean_object[:, i] = np.mean(object_vectors, axis = 1)
+    X_object = (vectors_object.T - mean_object).T
 
     print(X_universal.shape, X_object[0].shape)
