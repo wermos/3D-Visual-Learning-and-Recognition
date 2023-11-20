13f987dd58a0afb6c144d9e8dd66db86f93494df
paramrathour <rathourparam3435@gmail.com>
2023-11-20 19:37:52 +0530

    normalized eigenvectors returned by PCA function

--- pca.py
+++ pca.py
@@ -7,5 +7,6 @@ def PCA(datapoints):
     eigenvalues, eigenvectors = np.linalg.eig(L)
     num_components = np.searchsorted(np.cumsum(eigenvalues)/sum(eigenvalues), PCA_THRESHOLD, side = "left")+1
     eigenvectors = datapoints @ eigenvectors[:,:num_components]
+    eigenvectors = eigenvectors / np.linalg.norm(eigenvectors, axis = 0)
     eigenvalues = eigenvalues[:num_components]
     return eigenvalues, eigenvectors, num_components
