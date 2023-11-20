70b96a6fb6eb0baf57050663e70b349fb66bee14
paramrathour <rathourparam3435@gmail.com>
2023-11-20 21:55:30 +0530

    replaced `eig` by `eigh` for faster eigenvectors calculation and sorted eigenvectors as per their eigenvalues in decreasing order

--- pca.py
+++ pca.py
@@ -4,7 +4,9 @@ from constants import PCA_THRESHOLD
 
 def PCA(datapoints):
     L = datapoints.T @ datapoints
-    eigenvalues, eigenvectors = np.linalg.eig(L)
+    eigenvalues, eigenvectors = np.linalg.eigh(L)
+    eigenvalues = eigenvalues[::-1]
+    eigenvectors = eigenvectors[:, ::-1]
     num_components = np.searchsorted(np.cumsum(eigenvalues)/sum(eigenvalues), PCA_THRESHOLD, side = "left")+1
     eigenvectors = datapoints @ eigenvectors[:,:num_components]
     eigenvectors = eigenvectors / np.linalg.norm(eigenvectors, axis = 0)
