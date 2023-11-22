c40243dba9461e3cfc4b8dc73e45e742472a9582
wermos <63574588+wermos@users.noreply.github.com>
2023-11-22 19:12:51 +0530

    Updated `pca.py` and told it about the new program structure.

--- pca.py
+++ pca.py
@@ -1,15 +1,13 @@
 import numpy as np
 
-import constants
-
-def PCA(datapoints):
+def PCA(datapoints, img_size, pca_threshold):
     mean = np.mean(datapoints, axis = 1)
-    datapoints = datapoints - mean.reshape((constants.IMAGE_SIZE,1))
+    datapoints = datapoints - mean.reshape((img_size,1))
     L = datapoints.T @ datapoints
     eigenvalues, eigenvectors = np.linalg.eigh(L)
     eigenvalues = eigenvalues[::-1]
     eigenvectors = eigenvectors[:, ::-1]
-    num_components = np.searchsorted(np.cumsum(eigenvalues)/sum(eigenvalues), constants.PCA_THRESHOLD, side = "left")+1
+    num_components = np.searchsorted(np.cumsum(eigenvalues)/sum(eigenvalues), pca_threshold, side = "left")+1
     eigenvectors = datapoints @ eigenvectors[:,:num_components]
     eigenvectors = eigenvectors / np.linalg.norm(eigenvectors, axis = 0)
     eigenvalues = eigenvalues[:num_components]
