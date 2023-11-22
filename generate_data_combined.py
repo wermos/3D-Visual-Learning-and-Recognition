d31fe19e63ba5b7d5cae05a9098dddde4d984282
wermos <63574588+wermos@users.noreply.github.com>
2023-11-22 23:30:49 +0530

    Updated the file to tell it about the new program structure.

--- generate_data_combined.py
+++ generate_data_combined.py
@@ -1,71 +1,32 @@
-import sys
-from math import floor
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+from plot_util import generate_constants_list, plot_combined_graphs
 from test import process
-import constants
 
-def update_constants(pca_threshold, training_data_split):
-    constants.PCA_THRESHOLD = pca_threshold
-    constants.TRAINING_PERCENTAGE = training_data_split
-    constants.TESTING_PERCENTAGE = 1 - constants.TRAINING_PERCENTAGE
-    constants.NUM_TRAINING_IMAGES = floor(constants.NUM_IMAGES * constants.TRAINING_PERCENTAGE)
-    constants.NUM_TESTING_IMAGES = constants.NUM_IMAGES - constants.NUM_TRAINING_IMAGES
-
-if __name__ == "__main__":
-    sys.stdout = open('outputs/combined.txt','w')
-    pca_thresholds = np.arange(0.05,1,0.05)
-    training_data_splits = np.arange(0.05,1,0.05)
+def process_and_write(constants_tuple):
+    idx, jdx, constants = constants_tuple
     
-    accuracy_object = np.zeros((len(pca_thresholds), len(training_data_splits)))
-    accuracy_pose = np.zeros((len(pca_thresholds), len(training_data_splits)))
-    mean_error = np.zeros((len(pca_thresholds), len(training_data_splits)))
+    accuracy_object[idx][jdx], accuracy_pose[idx][jdx], mean_error[idx][jdx] = process(constants)   
     
-    for idx, pca_threshold in tqdm(list(enumerate(pca_thresholds)), desc="Generating data"):
-        for jdx, training_data_split in enumerate(training_data_splits):
-            update_constants(pca_threshold, training_data_split)
-            
-            accuracy_object[idx][jdx], accuracy_pose[idx][jdx], mean_error[idx][jdx] = process(False)
-            print(format(pca_threshold, ".2f"), format(training_data_split, ".2f"), format(accuracy_object[idx][jdx], ".3%"), format(accuracy_pose[idx][jdx], ".3%"), format(mean_error[idx][jdx], ".3f") + "\u00b0")
+    f = open('outputs/combined.txt', 'a')
+    f.write(f"{constants.PCA_THRESHOLD:.2f} {constants.TRAINING_PERCENTAGE:.2f} {accuracy_object[idx][jdx]:.3%} {accuracy_pose[idx][jdx]:.3%} {mean_error[idx][jdx]:.3f}\u00b0\n")
+    f.close()
 
-    pca_thresholds, training_data_splits = np.meshgrid(pca_thresholds, training_data_splits)
-    plots_directory = 'plots/combined/'
-    fig_1 = plt.figure()
-    ax_1 = plt.axes(projection ='3d')
-    ax_1.set_xlabel('PCA threshold')
-    ax_1.set_ylabel('Training Data Split')
-    ax_1.set_zlabel('Object Recognition Accuracy')
-    ax_1.set_xticks(np.linspace(0,1,6))
-    ax_1.set_yticks(np.linspace(0,1,6))
-    ax_1.set_zlim([0,1])
-    ax_1.set_title('Object Accuracy with varying parameters')
-    ax_1.plot_wireframe(pca_thresholds, training_data_splits, accuracy_object)
-    fig_1.savefig(plots_directory+'accuracy_object.pdf', dpi=200)
-    fig_1.savefig(plots_directory+'accuracy_object.png', dpi=200)
+pca_thresholds = np.arange(0.1,1,0.05)
+training_data_splits = np.arange(0.1,1,0.05)
 
-    fig_2 = plt.figure()
-    ax_2 = plt.axes(projection ='3d')
-    ax_2.set_xlabel('PCA threshold')
-    ax_2.set_ylabel('Training Data Split')
-    ax_2.set_zlabel('Pose Recognition Accuracy')
-    ax_2.set_xticks(np.linspace(0,1,6))
-    ax_2.set_yticks(np.linspace(0,1,6))
-    ax_2.set_zlim([0,1])
-    ax_2.set_title('Pose Accuracy with varying parameters')
-    ax_2.plot_wireframe(pca_thresholds, training_data_splits, accuracy_pose)
-    fig_2.savefig(plots_directory+'accuracy_pose.pdf', dpi=200)
-    fig_2.savefig(plots_directory+'accuracy_pose.png', dpi=200)
+accuracy_object = np.zeros((len(pca_thresholds), len(training_data_splits)))
+accuracy_pose = np.zeros((len(pca_thresholds), len(training_data_splits)))
+mean_error = np.zeros((len(pca_thresholds), len(training_data_splits)))
 
-    fig_3 = plt.figure()
-    ax_3 = plt.axes(projection ='3d')
-    ax_3.set_xlabel('PCA threshold')
-    ax_3.set_ylabel('Training Data Split')
-    ax_3.set_zlabel('Average pose error (in degrees)')
-    ax_3.set_xticks(np.linspace(0,1,6))
-    ax_3.set_yticks(np.linspace(0,1,6))
-    ax_3.set_title('Average pose error with varying parameters')
-    ax_3.plot_wireframe(pca_thresholds, training_data_splits, mean_error)
-    fig_3.savefig(plots_directory+'mean_error.pdf', dpi=200)
-    fig_2.savefig(plots_directory+'mean_error.png', dpi=200)
+constants_list = generate_constants_list(pca_thresholds, training_data_splits)
+
+# Clearing file content
+open('outputs/combined.txt','w').close()
+for constant in tqdm(constants_list, desc="Generating data"):
+    process_and_write(constant)
+
+print("Plotting and saving the plots...")
+plot_combined_graphs(pca_thresholds, training_data_splits, accuracy_object, accuracy_pose, mean_error)
+print("Finished.")
