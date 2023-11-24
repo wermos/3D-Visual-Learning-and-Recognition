2560f8449b811e2e1667e1b0c8bd4491c836c2fc
paramrathour <rathourparam3435@gmail.com>
2023-11-25 01:47:13 +0530

    replaced `sum` by `numpy.sum` in `pca.py`

--- pca.py
+++ pca.py
@@ -9,7 +9,7 @@ def PCA(datapoints):
     eigenvalues, eigenvectors = np.linalg.eigh(L)
     eigenvalues = eigenvalues[::-1]
     eigenvectors = eigenvectors[:, ::-1]
-    num_components = np.searchsorted(np.cumsum(eigenvalues)/sum(eigenvalues), constants.PCA_THRESHOLD, side = "left")+1
+    num_components = np.searchsorted(np.cumsum(eigenvalues)/np.sum(eigenvalues), constants.PCA_THRESHOLD, side = "left")+1
     eigenvectors = datapoints @ eigenvectors[:,:num_components]
     eigenvectors = eigenvectors / np.linalg.norm(eigenvectors, axis = 0)
     eigenvalues = eigenvalues[:num_components]
