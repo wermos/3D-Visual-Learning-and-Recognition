22377ca1bc46d08a380ee974f04abf24535df370
paramrathour <rathourparam3435@gmail.com>
2023-11-25 10:17:18 +0530

    added PCA plots and code

--- generate_data_pca.py
+++ generate_data_pca.py
@@ -1,46 +1,55 @@
-import os
-import sys
+# import os
+from os import environ
+environ['OMP_NUM_THREADS'] = '16'
+
+# import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from test import process
 import constants
+from plots_util import plot_accuracy, plot_accuracy_all_objects, plot_mean_error, plot_mean_error_all_objects, plot_error_histogram
 
-def update_constants(pca_threshold):
+def update_constants_pca_threshold(pca_threshold):
     constants.PCA_THRESHOLD = pca_threshold
 
+def update_constants_training_split(training_data_split):
+    constants.TRAINING_PERCENTAGE = training_data_split
+    constants.TESTING_PERCENTAGE = 1 - constants.TRAINING_PERCENTAGE
+    constants.NUM_TRAINING_IMAGES = floor(constants.NUM_IMAGES * constants.TRAINING_PERCENTAGE)
+    constants.NUM_TESTING_IMAGES = constants.NUM_IMAGES - constants.NUM_TRAINING_IMAGES
+
+def update_constants_combined(pca_threshold, training_data_split):
+    update_constants_pca_threshold(pca_threshold)
+    update_constants_training_split(training_data_split)
+
+update_constants = {0 : update_constants_pca_threshold, 1 : update_constants_training_split, 2 : update_constants_combined}
+
 if __name__ == "__main__":
-    sys.stdout = open('outputs/pca.txt','w')
+    # sys.stdout = open('outputs/pca.txt','w')
+    # pca_thresholds = np.arange(0.05,1,0.25)
     pca_thresholds = np.arange(0.05,1,0.05)
-    accuracy_object = np.zeros(len(pca_thresholds))
-    accuracy_pose = np.zeros(len(pca_thresholds))
-    mean_error = np.zeros(len(pca_thresholds))
+    accuracy_object = np.zeros((len(pca_thresholds), constants.NUM_OBJECTS))
+    accuracy_pose = np.zeros((len(pca_thresholds), constants.NUM_OBJECTS))
+    mean_error = np.zeros((len(pca_thresholds), constants.NUM_OBJECTS, constants.NUM_TESTING_IMAGES))
+    distances = np.zeros((len(pca_thresholds), constants.NUM_OBJECTS, constants.NUM_TESTING_IMAGES, 2))
     for idx, pca_threshold in tqdm(list(enumerate(pca_thresholds)), desc="Generating data"):
-        update_constants(pca_threshold)
-        accuracy_object[idx], accuracy_pose[idx], mean_error[idx] = process(False)
-        print(format(pca_threshold, ".2f"), format(accuracy_object[idx], ".3%"), format(accuracy_pose[idx], ".3%"), format(mean_error[idx], ".3f") + "\u00b0")
+        update_constants[0](pca_threshold)
+        accuracy_object[idx], accuracy_pose[idx], mean_error[idx], distances[idx] = process(False)
+        # print(format(pca_threshold, ".2f"), format(accuracy_object[idx], ".3%"), format(accuracy_pose[idx], ".3%"), format(mean_error[idx], ".3f") + "\u00b0")
 
-    plots_directory = 'plots/pca_threshold/'
-    fig_1, ax_1 = plt.subplots()
-    ax_1.set_xlabel('PCA threshold')
-    ax_1.set_xlim([0,1])
-    ax_1.set_xticks(np.linspace(0,1,11))
-    ax_1.set_ylabel('Accuracy')
-    ax_1.set_title('Accuracy with varying PCA Threshold')
-    ax_1.plot(pca_thresholds, accuracy_object, '-o', markersize=5)
-    ax_1.plot(pca_thresholds, accuracy_pose, '-o', markersize=5)
-    ax_1.set_ylim(bottom=0)
-    ax_1.legend(["Object Accuracy", "Pose Accuracy"])
-    fig_1.savefig(plots_directory+'accuracy.pdf', dpi=200)
-    fig_1.savefig(plots_directory+'accuracy.png', dpi=200)
+    plot_accuracy(pca_thresholds, np.mean(accuracy_object, axis=1)/constants.NUM_TESTING_IMAGES, np.mean(accuracy_pose, axis=1)/constants.NUM_TESTING_IMAGES, -1, 0)
+    plot_accuracy_all_objects(pca_thresholds, accuracy_object/constants.NUM_TESTING_IMAGES, 0, 0)
+    plot_accuracy_all_objects(pca_thresholds, accuracy_pose/constants.NUM_TESTING_IMAGES, 1, 0)
+    for object_id in range(constants.NUM_OBJECTS):
+        plot_accuracy(pca_thresholds, accuracy_object[:,object_id]/constants.NUM_TESTING_IMAGES, accuracy_pose[:,object_id]/(constants.NUM_TESTING_IMAGES), object_id, 0)
 
-    fig_2, ax_2 = plt.subplots()
-    ax_2.set_xlabel('PCA threshold')
-    ax_2.set_xlim([0,1])
-    ax_2.set_xticks(np.linspace(0,1,11))
-    ax_2.set_ylabel('Average pose error (in degrees)')
-    ax_2.set_title('Average pose error with varying PCA Threshold')
-    ax_2.plot(pca_thresholds, mean_error, '-o', markersize=5)
-    fig_2.savefig(plots_directory+'mean_error.pdf', dpi=200)
-    fig_2.savefig(plots_directory+'mean_error.png', dpi=200)
+    plot_mean_error(pca_thresholds, np.mean(mean_error, axis=(1,2)), -1, 0)
+    plot_mean_error_all_objects(pca_thresholds, np.mean(mean_error, axis=2), 0)
+    for object_id in range(constants.NUM_OBJECTS):
+        plot_mean_error(pca_thresholds, np.mean(mean_error[:,object_id], axis=1), object_id, 0)
+
+    plot_error_histogram(np.repeat(pca_thresholds, constants.NUM_OBJECTS*constants.NUM_TESTING_IMAGES), mean_error.reshape(len(pca_thresholds)*constants.NUM_OBJECTS*constants.NUM_TESTING_IMAGES) , -1, 0)
+    for object_id in range(constants.NUM_OBJECTS):
+        plot_error_histogram(np.repeat(pca_thresholds, constants.NUM_TESTING_IMAGES), mean_error[:,object_id].reshape(len(pca_thresholds)*constants.NUM_TESTING_IMAGES), object_id, 0)
---
+++ plots_util.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+import constants
+
+plots_directory = 'plots/coil-' + str(constants.NUM_OBJECTS)
+
+title_map = {0: 'PCA Threshold', 1: 'Training Data Split', 2: 'PCA Threshold and Training Data Split'}
+title_directory_map = {0: '/pca_threshold/', 1: '/training_data_split/',  2: 'combined'}
+accuracy_map = {0: 'Object', 1: 'Pose'}
+accuracy_directory_map = {0: 'object', 1: 'pose'}
+
+def save_fig(figure, filename):
+    figure.savefig(plots_directory + filename + '.pdf', dpi=200)
+    figure.savefig(plots_directory + filename + '.png', dpi=200)
+
+def plot_accuracy(x_axis, y_axis_1, y_axis_2, object_id, plot_type):
+    object_name = 'overall' if object_id == -1 else 'object_' + str(object_id)
+    fig, ax = plt.subplots()
+    ax.set_xlabel(title_map[plot_type])
+    ax.set_xlim([0,1])
+    ax.set_xticks(np.linspace(0,1,11))
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Accuracy ' + object_name + ' with varying ' + title_map[plot_type])
+    ax.plot(x_axis, y_axis_1, '-o', markersize=5)
+    ax.plot(x_axis, y_axis_2, '-o', markersize=5)
+    ax.set_ylim(bottom=0)
+    ax.legend(["Object Accuracy", "Pose Accuracy"])
+    save_fig(fig, title_directory_map[plot_type] + 'accuracy_'+ object_name)
+    plt.close()
+
+def plot_accuracy_all_objects(x_axis, y_axis, accuracy_type, plot_type):
+    fig, ax = plt.subplots()
+    ax.set_xlabel(title_map[plot_type])
+    ax.set_xlim([0,1])
+    ax.set_xticks(np.linspace(0,1,11))
+    ax.set_ylabel(accuracy_map[accuracy_type] + ' Accuracy')
+    ax.set_title(accuracy_map[accuracy_type] + ' Accuracy for all objects with varying ' + title_map[plot_type])
+    ax.plot(x_axis, y_axis, '-o', markersize=5)
+    ax.set_ylim(bottom=0)
+    ax.legend(['object ' + str(object_id) for object_id in range(constants.NUM_OBJECTS)], prop={'size': 2})
+    save_fig(fig, title_directory_map[plot_type] + accuracy_directory_map[accuracy_type] + '_accuracy_all_objects')
+    plt.close()
+
+def plot_mean_error(x_axis, y_axis, object_id, plot_type):
+    object_name = 'overall' if object_id == -1 else 'object_' + str(object_id)
+    fig, ax = plt.subplots()
+    ax.set_xlabel(title_map[plot_type])
+    ax.set_xlim([0,1])
+    ax.set_xticks(np.linspace(0,1,11))
+    ax.set_ylabel('Average pose error (magnitude)')
+    ax.set_title('Average pose error ' + object_name + ' with varying ' + title_map[plot_type])
+    ax.plot(x_axis, y_axis, '-o', markersize=5)
+    ax.set_ylim(bottom=0)
+    save_fig(fig, title_directory_map[plot_type] + 'mean_error_'+ object_name)
+    plt.close()
+
+def plot_mean_error_all_objects(x_axis, y_axis, plot_type):
+    fig, ax = plt.subplots()
+    ax.set_xlabel(title_map[plot_type])
+    ax.set_xlim([0,1])
+    ax.set_xticks(np.linspace(0,1,11))
+    ax.set_ylabel('Average pose error (magnitude)')
+    ax.set_title('Average pose error for all objects with varying ' + title_map[plot_type])
+    ax.plot(x_axis, y_axis, '-o', markersize=5)
+    ax.set_ylim(bottom=0)
+    ax.legend(['object ' + str(object_id) for object_id in range(constants.NUM_OBJECTS)], prop={'size': 2})
+    save_fig(fig, title_directory_map[plot_type]  + 'mean_error_all_objects')
+    plt.close()
+
+def plot_error_histogram(x_axis, y_axis, object_id, plot_type):
+    object_name = 'overall' if object_id == -1 else 'object_' + str(object_id)
+    fig, ax = plt.subplots()
+    ax.set_xlabel(title_map[plot_type])
+    ax.set_ylabel('Pose error (magnitude)')
+    ax.set_title('Pose error ' + object_name + ' with varying ' + title_map[plot_type])
+    _, _, _, image = ax.hist2d(x_axis, y_axis, norm=LogNorm())
+    fig.colorbar(image)
+    ax.set_ylim(bottom=0)
+    save_fig(fig, title_directory_map[plot_type] + 'error_histogram_'+ object_name)
+    plt.close()
