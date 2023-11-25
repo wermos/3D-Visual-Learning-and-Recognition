79607d0165c3f6dd18de42ff48ed6933cc768354
paramrathour <rathourparam3435@gmail.com>
2023-11-25 05:48:47 +0530

    revamped return variables to help detailed plotting

--- test.py
+++ test.py
@@ -30,31 +30,30 @@ def test_image(image, mean_universal, mean_object, eigenvectors_universal, eigen
     distance.append(distances[angle_id][0])
     return object_id, angle_values[angle_id], distance
 
-def process(DEBUGGING = False):
-    print("data loading initiated") if DEBUGGING else None
+def process(DEBUGGING = False, precision = 5):
+    print("data loading initiated...") if DEBUGGING else None
     training, testing = data_loader()
-    print("data loading completed...training initiated") if DEBUGGING else None
+    print("data loading completed\ntraining initiated...") if DEBUGGING else None
     mean_universal, mean_object, eigenvectors_universal, eigenvectors_object, manifolds_universal, manifolds_object = train_model(training)
-    print("training completed...testing initiated") if DEBUGGING else None
-    precision = 5
+    print("training completed\ntesting initiated...") if DEBUGGING else None
     angle_values = np.arange(0,360,precision)
     manifold_points_universal, manifold_points_object = evaluate_cubic_splines_for_angles(manifolds_universal, manifolds_object, angle_values)
     num_tests = len(testing)
-    accurate_count = np.zeros(2)
-    error = 0
+    accurate_count = np.zeros((constants.NUM_OBJECTS, 2))
+    error = np.zeros((constants.NUM_OBJECTS, constants.NUM_TESTING_IMAGES))
+    distances = np.zeros((constants.NUM_OBJECTS, constants.NUM_TESTING_IMAGES, 2))
     # print("Actual vs Estimated") if DEBUGGING else None
-    for object_id_true, angle_true , image in testing:
-        object_id, angle, _ = test_image(image, mean_universal, mean_object, eigenvectors_universal, eigenvectors_object, manifold_points_universal, manifold_points_object, angle_values)
+    for idx, (object_id_true, angle_true, image) in enumerate(testing):
+        object_id, angle, distances[object_id_true][idx % constants.NUM_TESTING_IMAGES] = test_image(image, mean_universal, mean_object, eigenvectors_universal, eigenvectors_object, manifold_points_universal, manifold_points_object, angle_values)
         # print("Object:", [object_id_true, object_id], "Angle:", [angle_true, angle]) if DEBUGGING else None
-        accurate_count[0] += (object_id_true == object_id)
-        accurate_count[1] += (angle_true == angle)*(object_id_true == object_id)
-        error += min(abs(angle_true - angle), 360-abs(angle_true - angle))*(object_id_true == object_id)
+        accurate_count[object_id_true][0] += (object_id_true == object_id)
+        accurate_count[object_id_true][1] += (angle_true == angle)#*(object_id_true == object_id)
+        error[object_id_true][idx % constants.NUM_TESTING_IMAGES] = min(abs(angle_true - angle), 360-abs(angle_true - angle))#*(object_id_true == object_id)
     print("testing completed...generating stats\n") if DEBUGGING else None
-    return accurate_count[0]/num_tests, accurate_count[1]/accurate_count[0], error/accurate_count[0]
+    return accurate_count[:,0], accurate_count[:,1], error, distances
 
 if __name__ == "__main__":
-    accuracy_object, accuracy_pose, mean_error = process(True)
-    print("Object Recognition accuracy: ", format(accuracy_object, ".3%"))
-    print("Pose Estimation accuracy:", format(accuracy_pose, ".3%"))
-    print("Mean Pose error:", format(mean_error, ".3f") + "\u00b0")
-    
+    accuracy_object, accuracy_pose, mean_error, _ = process(True)
+    print("Object Recognition accuracy: ", format(np.sum(accuracy_object)/(constants.NUM_OBJECTS*constants.NUM_TESTING_IMAGES), ".3%"))
+    print("Pose Estimation accuracy:", format(np.sum(accuracy_pose)/(constants.NUM_OBJECTS*constants.NUM_TESTING_IMAGES), ".3%"))
+    print("Mean Pose error:", format(np.sum(mean_error)/(constants.NUM_OBJECTS*constants.NUM_TESTING_IMAGES), ".3f") + "\u00b0")
